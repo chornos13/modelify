389038a93162190a88e7c8eb0257fd3eef46cfde
chornos13 <chornos13@hotmail.com>
2019-06-26 10:26:20 +0700

    remove unused code

--- index.js
+++ index.js
@@ -6,10 +6,3 @@ const argv = require('yargs')
     Crudify.generateModelFrontend(yargs.argv.path)
   })
   .argv
-
-
-if (argv.ships > 3 && argv.distance < 53.5) {
-  console.log('Plunder more riffiwobbles!')
-} else {
-  console.log('Retreat from the xupptumblers!')
-}
